dfb6ba33decdfe7a69f2b106234289de4bd06d5b
Nahia Akter <nhaktr@gmail.com>
2020-07-03 13:43:56 -0400

    third commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -26,32 +26,19 @@
             anchor.className = "nav-link active";
         }
        }
-
-       /* step 1 hook into the spot (element) on the page
-       let jumbotron = document.getElementsByClassName("jumbotron")[0];
-
-       // step 2 create a new element
-       let newParagraph = document.createElement("p");
-       
-       // step 3 configure the new element
-       newParagraph.textContent = title;
-
-       // step 4 attach the new element
-       jumbotron.appendChild(newParagraph);*/
-
     } 
-    document.getElementById("p1").innerHTML = 
+    
+    document.getElementById("pro1").innerHTML = 
        "My first project was developing a website for a real state agent."+
        "Realtor's website is literally their face on the world wide web. So it needs to be attractive."+
        "There was the information about the houses for sale including their details pictures and price.";
-    document.getElementById("p3").innerHTML = 
+    document.getElementById("pro3").innerHTML = 
         "Currently I'm working on SRS documentation in Software Engineering Metholody."+
         "A software requirements specification is the basis for the entire project."+
         "It used to provide critical information to multiple teams:development, quality assurance, operations, and maintenance."
-    document.getElementById("p2").innerHTML = 
+    document.getElementById("pro2").innerHTML = 
         "I worked on pandas in a team. pandas is a fast, powerful, flexible and easy to use open source data analysis"+
         "and manipulation tool, built on top of the Python programming language.this semester I'm going to learn more about Python."
-
 
     window.addEventListener("load", Start);
 
